7b892c1538b4da0f4d08d4642f2d59f71a3b542c
Krystof Pesek <christoffon@gmail.com>
2011-05-10 14:40:07 +0200

    pureJava branch

---
+++ javaPreview/build/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -classpath .:/home/kof/p5/build/cmd/processing-cmd-0191/lib/core.jar:/home/kof/sketchBook/2010/08-srpen/FreeFOrm/applet/PeasyCam.jar:/sketchBook/libraries/oscP5/library/* Main
